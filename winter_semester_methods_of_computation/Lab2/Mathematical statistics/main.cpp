5fdefb8af3a68f96df7e7e6efca6b1132f9d1c4c
Феодор Карпов <fedorkarpov1312@gmail.com>
2021-04-14 17:14:36 +0300

    Implement writing and reading to a file of 14 400 vectors of size 9

---
+++ winter_semester_methods_of_computation/Lab2/Mathematical statistics/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+
+    cout<<a;
+    vector<double> Sequence;
+
+    for(int i=0;i<14 400; i++)
+    {
+        double a = (double)rand()/RAND_MAX;
+        Sequence.at(a);
+    }
+    return 0;
+}
