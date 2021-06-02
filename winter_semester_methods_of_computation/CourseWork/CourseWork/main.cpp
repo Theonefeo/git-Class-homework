3cd2f1c10c97bad53af9715ff2d0b47fd1b33cf9
Феодор Карпов <fedorkarpov1312@gmail.com>
2021-06-02 16:13:13 +0300

    Implement a file recording of the value of the angle of deflection of the pendulum at different values of the amplitude. Find the maximum angle in each mode.

--- winter_semester_methods_of_computation/CourseWork/CourseWork/main.cpp
+++ winter_semester_methods_of_computation/CourseWork/CourseWork/main.cpp
@@ -1,42 +1,91 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
-
 
 using namespace std;
 
-solution solve_de(double eps)
+vector <double> runge_kutta(double h)
 {
-    double real_eps, h=1;
-    do {
-        fi1[] = runge_kutta(h);
-        fi2[] = runge_kutta(h/2);
+    vector <double> fi;
+    vector <double> psi;
+    double g = 9.8;
+    double w = 3.130495;
+    double ti;
+    double Sfi;
+    double Spsi;
 
-        real_eps = radian_error_to_mm(pravilo_runge(fi1, fi2, p = 2)); {|x(h/2) - x(h)|/(2^p - 1) } //pravilo_runge - вывод и сравнение результатов таблиц (не забыть про max d = max D/3), перевод радиан в мм то есть нахождение расхождения
-        h = h/2;
-    } while (real_eps >= eps);
+    double A=2;
+    fi= {0};
+    psi= {0};
 
-    return fi2, real_eps, h;
+
+    for(int i=1; i<30/h+1; i++)
+    {
+        ti = i*h;
+        double m = (((-g)*sin(fi.back()))+(A*sin(w*ti)*cos(fi.back())));
+
+        Sfi = (fi.back()+(h*psi.back()));
+        Spsi = (psi.back()+(h*m));
+        fi.push_back(fi.back() + (h*(psi.back()+Spsi))*0.5);
+        ti=(i+1)*h;
+        double n = (((-g)*sin(Sfi))+(A*sin(w*ti)*cos(Sfi)));
+        psi.push_back(psi.back()+(h*(m+n))*0.5);
+    }
+
+
+    return fi;
 }
-
-
-int main()
+double pravilo_runge(vector <double> fi1, vector <double>  fi2, int p = 2)
 {
-    eps = 1;
-    fi[], real_eps, h = solve_de(eps);
-    cout << "Solved with eps={real_eps} and h={h}" << endl;
-    save_to_file(fi);
-
-    cout << "Max angle: " << abs(fi[]).max();
-
-
-
-
-    cout << "Hello world!" << endl;
-    for(int i=0; i<30; i++)
+    double element_runge;
+    double Max = 0;
+    for(unsigned i=0; i<fi1.size(); i++)
     {
 
+        element_runge = (abs(fi1.at(i) - fi2.at(i*p))/(pow(2.,p) - 1));
 
+        if(Max<element_runge)
+            Max = element_runge;
     }
+    cout<<"M= "<<Max<<endl;
+    return Max;
+}
+double radian_error_to_mm(double element_runge)
+{
+
+    double r;
+    r = sqrt(2-2*(cos(element_runge)));
+    return r;
+}
+vector <double> solve_de(double eps, double &real_eps, double &h)
+{
+    vector <double> fi1;
+    vector <double> fi2;
+
+    h=0.01;
+    do
+    {
+        fi1 = runge_kutta(h);
+        fi2 = runge_kutta(h/2);
+        real_eps = radian_error_to_mm(pravilo_runge(fi1, fi2, 2));
+        h = h/2;
+    }
+    while (real_eps >= eps);
+
+    return fi2;
+}
+int main()
+{
+    double eps = 1;
+    vector <double> Fi;
+    double Real_eps, h;
+
+    Fi = solve_de(eps, Real_eps, h);
+
+    cout<<"Solved with eps="<< Real_eps<< "h=" << h << endl;
+
+    save_to_file(Fi);
+    cout << "Max angle: " << abs(Fi).max();
 
 
     return 0;
