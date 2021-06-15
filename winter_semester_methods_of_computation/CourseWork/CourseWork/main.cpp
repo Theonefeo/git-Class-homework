42208fb6a5268e33562b52cbba779da2f8388d68
Феодор Карпов <fedorkarpov1312@gmail.com>
2021-06-15 16:33:13 +0300

    CourseWork id done. Implemented the recording of the results of the pendulum rolling at 3 different forces in the files. The tilt angles for each mode are found.

--- winter_semester_methods_of_computation/CourseWork/CourseWork/main.cpp
+++ winter_semester_methods_of_computation/CourseWork/CourseWork/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
-vector <double> runge_kutta(double h)
+vector <double> runge_kutta(double h, double A)
 {
     vector <double> fi;
     vector <double> psi;
@@ -14,7 +15,6 @@ vector <double> runge_kutta(double h)
     double Sfi;
     double Spsi;
 
-    double A=2;
     fi= {0};
     psi= {0};
 
@@ -47,7 +47,7 @@ double pravilo_runge(vector <double> fi1, vector <double>  fi2, int p = 2)
         if(Max<element_runge)
             Max = element_runge;
     }
-    cout<<"M= "<<Max<<endl;
+    cout<<endl<<"M= "<<Max<<endl;
     return Max;
 }
 double radian_error_to_mm(double element_runge)
@@ -57,7 +57,7 @@ double radian_error_to_mm(double element_runge)
     r = sqrt(2-2*(cos(element_runge)));
     return r;
 }
-vector <double> solve_de(double eps, double &real_eps, double &h)
+vector <double> solve_de(double eps, double &real_eps, double &h, double A)
 {
     vector <double> fi1;
     vector <double> fi2;
@@ -65,8 +65,8 @@ vector <double> solve_de(double eps, double &real_eps, double &h)
     h=0.01;
     do
     {
-        fi1 = runge_kutta(h);
-        fi2 = runge_kutta(h/2);
+        fi1 = runge_kutta(h, A);
+        fi2 = runge_kutta(h/2, A);
         real_eps = radian_error_to_mm(pravilo_runge(fi1, fi2, 2));
         h = h/2;
     }
@@ -74,19 +74,91 @@ vector <double> solve_de(double eps, double &real_eps, double &h)
 
     return fi2;
 }
+void save_to_file(vector <double> Fi, int i)
+{
+        ofstream fout;
+        switch(i)
+        {
+            case 0:
+            {
+                fout.open("Angle_Fi2.txt");
+                break;
+            }
+            case 1:
+            {
+                fout.open("Angle_Fi1.txt");
+                break;
+            }
+            case 2:
+            {
+                fout.open("Angle_Fi0.5.txt");
+                break;
+            }
+            default:
+            {
+                cout<<"WRONG";
+                break;
+            }
+        }
+    for(unsigned ii=0; ii<Fi.size(); ii++)
+    {
+        fout<<Fi.at(ii)<<" "<<endl;
+    }
+
+    fout.close();
+}
+double Max_angle(vector <double> Fi)
+{
+    double Max = 0;
+    for(unsigned i=0; i<Fi.size(); i++)
+    {
+        if(Max<Fi.at(i))
+        Max = Fi.at(i);
+    }
+    return Max;
+}
 int main()
 {
-    double eps = 1;
-    vector <double> Fi;
-    double Real_eps, h;
+    int i=0;
+    double A;
+    do{
+        switch(i)
+        {
+            case 0:
+            {
+                A = 2;
+                break;
+            }
+            case 1:
+            {
+                A = 1;
+                break;
+            }
+            case 2:
+            {
+                A = 0.5;
+                break;
+            }
+            default:
+            {
+                cout<<"WRONG";
+                break;
+            }
+        }
 
-    Fi = solve_de(eps, Real_eps, h);
+        double eps = 1;
+        vector <double> Fi;
+        double Real_eps, h;
 
-    cout<<"Solved with eps="<< Real_eps<< "h=" << h << endl;
+        Fi = solve_de(eps, Real_eps, h, A);
 
-    save_to_file(Fi);
-    cout << "Max angle: " << abs(Fi).max();
+        cout<<endl<<"Solved with eps="<< Real_eps<< "h=" << h << endl;
 
+        save_to_file(Fi, i);
+        cout <<endl<< "Max angle: " <<Max_angle(Fi);
+     i++;
+    }
+    while(i!=3);
 
     return 0;
 }
