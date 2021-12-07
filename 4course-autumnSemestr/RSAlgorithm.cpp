198425a3bc6376c5cd48b00b7f66dd128a688a77
Theonefeo <fedorkarpov1312@gmail.com>
2021-12-07 13:16:53 +0300

    RSA algorithm.

---
+++ 4course-autumnSemestr/RSAlgorithm.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+void publicKey()
+{
+
+}
+void privateKey()
+{
+    
+}
+void encryption()
+{
+    
+}
+void decryption()
+{
+    
+}
+
+int main()
+{
+    publicKey();
+    privateKey();
+    encryption();
+    decryption();
+
+    return 0;
+}
